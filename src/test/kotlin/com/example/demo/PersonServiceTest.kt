60be2efeb244b697f8105b5272778e9af88e82f1
Ankit Gaurav <theankitgaurav@gmail.com>
2023-07-31 22:20:09 +0530

    Separate test for caffeine cache

--- src/test/kotlin/com/example/demo/PersonServiceTest.kt
+++ src/test/kotlin/com/example/demo/PersonServiceTest.kt
@@ -1,15 +1,22 @@
 package com.example.demo
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.cache.CacheManager
+import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Primary
 
 
 @SpringBootTest
-@Import(CacheConfiguration::class)
+@Import(CaffeineCacheConfiguration::class)
 class PersonServiceTest {
 
     @Autowired
@@ -19,20 +26,44 @@ class PersonServiceTest {
     private lateinit var cacheManager: CacheManager
 
     @Test
-    fun `test cache`() {
+    fun `cache should not hold more than max weight in bytes`() {
         service.fetch("person1")
         service.fetch("person2")
-        service.fetch("person2")
-        service.fetch("person3")
-        service.fetch("person3")
         service.fetch("person3")
 
         val cache = cacheManager.getCache("personCache")
         Thread.sleep(1000)
 
         val caffeineCache = cache?.nativeCache as Cache<*, *>
-        println(caffeineCache.asMap())
+
+        Assertions.assertEquals(caffeineCache.asMap().size, 2)
         println("Stats: ${caffeineCache.stats()}")
     }
 }
 
+
+@TestConfiguration
+class CaffeineCacheConfiguration {
+    @Bean
+    fun caffeineConfig(): Caffeine<*, *> {
+        return Caffeine.newBuilder()
+            .maximumWeight(20)
+            .evictionListener { key: Any?, _: Any?, cause -> println("Evicted key $key due to $cause") }
+//            .weigher { _: Any, v: Any -> v.toByteArray().size }
+            .weigher { _: Any, _: Any -> 10 }
+            .recordStats()
+    }
+
+    @Bean
+    @Primary
+    fun cacheManagerCaffeine(caffeine: Caffeine<Any, Any>): CacheManager {
+        val caffeineCacheManager = CaffeineCacheManager()
+        caffeineCacheManager.setCaffeine(caffeine)
+        return caffeineCacheManager
+    }
+
+    private fun Any.toByteArray(): ByteArray {
+        return jacksonObjectMapper().writeValueAsBytes(this)
+            .also { println("Weight of $this is ${it.size}") }
+    }
+}
